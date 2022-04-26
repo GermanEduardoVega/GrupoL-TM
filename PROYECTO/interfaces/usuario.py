a5b50a94c953fdc4533cfa2135342fd48656d614
Araya Alejo <alejo.araya@alumnos.frm.utn.edu.ar>
2022-04-26 17:18:00 -0300

    interfaz grafica de registrar usuario

--- PROYECTO/interfaces/usuario.py
+++ PROYECTO/interfaces/usuario.py
@@ -1,3 +1,8 @@
+from tkinter import *
+from functools import partial
+import tkinter as tk
+from tkinter import messagebox
+
 class Usuario:
     def __init__ (self, _nombre, _pasaporte, _cuil, _licencia, _edad, _foto):
         self._nombre = _nombre
@@ -44,3 +49,66 @@ class Usuario:
 
     def set_foto(self, new_foto):
         self._foto = new_foto
+
+
+class Ventana:
+    def __init__(self, inter):
+        self.interfaz = inter
+        self.interfaz.geometry("800x600")
+        self.interfaz.title("Alquila YA")
+        self.dibujarVentana()
+
+# LAYOUT
+    def dibujarVentana(self):
+        Button(self.interfaz, command=self.loginAdmin,
+               text="Login Admin").place(x=700, y=10)
+        Button(self.interfaz, command=self.alquilar,
+               text="Alquilar").place(x=325, y=250)
+        Button(self.interfaz, command=self.devolucion,
+               text="Devolucion").place(x=425, y=250)
+
+# VALIDACION
+    def validar(a1, a2):
+        #print("hola")
+        if a1 == "Rxvargas" and a2 == "rodrigo":
+            abrirVentanaAdmin()
+        else:
+            messagebox.showwarning("Error", "Error de credenciales")
+
+    def loginAdmin(self):
+        self.interfaz.withdraw()
+        ventana2 = tk.Toplevel()
+        ventana2.geometry("800x600")
+        ventana2.title("Alquila YA")
+        etiqueta1 = Label(ventana2, text="Nombre: ").place(x=325, y=150)
+        text1 = Entry(ventana2).place(x=400, y=150)
+        etiqueta2 = Label(ventana2, text="Pasaporte: ").place(x=325, y=175)
+        text2 = Entry(ventana2).place(x=400, y=175)
+        etiqueta3 = Label(ventana2, text="Cuil: ").place(x=325, y=200)
+        text2 = Entry(ventana2).place(x=400, y=200)
+        etiqueta4 = Label(ventana2, text="Liciencia: ").place(x=325, y=225)
+        text2 = Entry(ventana2).place(x=400, y=225)
+        etiqueta5 = Label(ventana2, text="Edad: ").place(x=325, y=250)
+        text2 = Entry(ventana2).place(x=400, y=250)
+        Button(self.interfaz, command=self.loginAdmin,
+               text="Reconocimiento facial").place(x=700, y=350)
+
+        botonValidar = Button(ventana2, text="Validar",
+                              command=partial(validar, text1, text2)).place(x=450, y=350)
+
+    def abrirVentanaAdmin(self):
+        ventana2.withdraw()
+        ventana3 = tk.Toplevel()
+        ventana3.geometry("800x600")
+        ventana3.title("Alquila YA")
+        Label(ventana3, text="Exito!!!").place(x=325, y=250)
+
+    def alquilar(self):
+        print("Hola")
+
+    def devolucion(self):
+        print("Hola")
+
+
+obj = Ventana(Tk())
+obj.interfaz.mainloop()
