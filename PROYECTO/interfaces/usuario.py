4d9c415b87a4efd8de7b769fd4ee5e57861f9806
Araya Alejo <alejo.araya@alumnos.frm.utn.edu.ar>
2022-04-26 17:05:14 -0300

    Getters and Setters

--- PROYECTO/interfaces/usuario.py
+++ PROYECTO/interfaces/usuario.py
@@ -11,14 +11,36 @@ class Usuario:
     def get_nombre(self):
         return self._nombre
 
+    def get_pasaporte(self):
+        return self._pasaporte
+
+    def get_cuil(self):
+        return self._cuil
+
+    def get_licencia(self):
+        return self._licencia
+
+    def get_edad(self):
+        return self._edad
+
+    def get_foto(self):
+        return self._foto
+
+
     def set_nombre(self, new_nombre):
         self._nombre = new_nombre
 
+    def set_pasaporte(self, new_pasaporte):
+        self._pasaporte = new_pasaporte
 
+    def set_cuil(self, new_cuil):
+        self._cuil = new_cuil
 
+    def set_licencia(self, new_licencia):
+        self._licencia = new_licencia
 
-usuario = Usuario('ARAYA ALEJO', 25342534, 20-42749595-8, '123ads24213', 21,"FOTO")
+    def set_edad(self, new_edad):
+        self._edad = new_edad
 
-
-
-print(usuario.get_nombre())
+    def set_foto(self, new_foto):
+        self._foto = new_foto
